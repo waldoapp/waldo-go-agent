ccf1e6dce1a3487f6444b747c508f39659b4ea3e
Julien Roncaglia <j.roncaglia@tricentis.com>
2024-03-15 10:41:47 +0100

    Add tests for build URL encoding too

--- upload_test.go
+++ upload_test.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/url"
 	"testing"
 )
 
-func TestCorrectErrorPayloadEncoding(t *testing.T) {
+func TestErrorPayloadEncoding(t *testing.T) {
 	ua := newUploadAction("/some/path", "token", "variant", "f5ebaa009c043737f30bcb0f53d7614d09968e00",
 		"user\"my-branch", false, make(map[string]string))
 
@@ -29,5 +30,30 @@ func TestCorrectErrorPayloadEncoding(t *testing.T) {
 	}
 	if jsonDecoded["message"] != "The following failed: \"some error message\"" {
 		t.Errorf("Expected error to be 'The following failed: \"some error message\"', but got '%v'", jsonDecoded["message"])
+	}
+}
+
+func TestBuildURLEncoding(t *testing.T) {
+	ua := newUploadAction("/some/path", "token", "variant", "f5ebaa009c043737f30bcb0f53d7614d09968e00",
+		"user\"=+my-branch", false, make(map[string]string))
+
+	ua.gitInfo = &gitInfo{
+		branch: "user\"=+my-branch",
+		access: ok,
+	}
+
+	ua.ciInfo = &ciInfo{
+		gitBranch: "user\"=+my-branch",
+	}
+
+	output := ua.makeBuildURL()
+
+	parsed, err := url.ParseRequestURI(output)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if parsed.Query().Get("ciGitBranch") != "user\"=+my-branch" {
+		t.Errorf("Expected branch to be 'user\"=+my-branch', but got '%v'", parsed.Query().Get("ciGitBranch"))
 	}
 }
